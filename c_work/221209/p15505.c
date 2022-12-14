3f2ab5fde7f61a91b73b88147c017df03c91f0a6
bholic <bholic0990@naver.com>
2022-12-14 13:53:38 +0900

    221214

--- c_work/221209/p15505.c
+++ c_work/221209/p15505.c
@@ -4,18 +4,18 @@ int main()
 {
     int a,n,b,t;
     a=0;
-    t =0;
     b=0;
     n=0;
     printf("정수는 몇개를 입력할까요?");
     scanf("%d",&n);
 
-    while(a<5)
+    while(a<=n)
     {
-        printf("정수선택:");
-        scanf("%lf",&b);
-        t++;
-        a++;
+         printf("정수선택:");
+          scanf("%lf",&b);
+         b++;
+         a++;
+         t = t + b;
     }
     printf("합은 %lf",t);
 
